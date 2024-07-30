abaf3f1f11a51963ea69db8f5f0c1c206a700c2a
Mujib Halimi <salimjan3008@gmail.com>
2024-07-31 01:52:55 +0430

    the messaage refactored to a nicer format

--- src/app/pages/posts/page.tsx
+++ src/app/pages/posts/page.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { useState } from 'react';
-import { useQuery, useMutation, useQueryClient, HydrationBoundary, DehydratedState } from '@tanstack/react-query';
+import { useState } from "react";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  HydrationBoundary,
+  DehydratedState,
+} from "@tanstack/react-query";
 import { fetchPosts, deletePost } from "@/app/actions/postAction";
-import Add from './add/page';
+import Add from "./add/page";
 
 type Post = {
   id: number;
@@ -25,11 +31,11 @@ export default function App({ dehydratedState }: AppProps) {
   const [pageSize, setPageSize] = useState(10);
   const [dataSource, setDataSource] = useState({});
   const [isMessage, setIsMessage] = useState(false);
-  const [messageData, setMessageData] = useState('');
+  const [messageData, setMessageData] = useState("");
   const queryClient = useQueryClient();
 
-  const { data, error } = useQuery<{ data: Post[], pagination: Pagination }>({
-    queryKey: ['posts', page, pageSize],
+  const { data, error } = useQuery<{ data: Post[]; pagination: Pagination }>({
+    queryKey: ["posts", page, pageSize],
     queryFn: () => fetchPosts(page.toString(), pageSize.toString()),
   });
 
@@ -39,9 +45,9 @@ export default function App({ dehydratedState }: AppProps) {
   const deleteMutation = useMutation({
     mutationFn: (postId: number) => deletePost(postId),
     onSuccess: () => {
-      console.log('Post deleted successfully');
+      console.log("Post deleted successfully");
       queryClient.invalidateQueries({
-        queryKey: ['posts', page, pageSize],
+        queryKey: ["posts", page, pageSize],
       });
     },
   });
@@ -49,10 +55,10 @@ export default function App({ dehydratedState }: AppProps) {
   const handleDelete = (post: Post) => {
     setIsMessage(true);
     deleteMutation.mutate(post.id);
-    setMessageData('Post Deleted Successfully');
+    setMessageData("Post Deleted Successfully");
     setTimeout(() => {
       setIsMessage(false);
-      setMessageData('');
+      setMessageData("");
     }, 1500);
   };
 
@@ -80,10 +86,31 @@ export default function App({ dehydratedState }: AppProps) {
         <div className="w-full p-8 bg-white rounded-lg shadow-lg">
           <div className="flex justify-between items-center mb-4">
             <h1 className="text-3xl font-bold text-gray-800 mt-6">All Posts</h1>
-            {isMessage && <span className='text-2xl font-bold text-black bg-green-400 p-2 rounded-lg'>{messageData}</span>}
+            {isMessage && (
+              <div className="flex items-center justify-center bg-green-400 rounded-lg p-2">
+                <svg
+                  className="w-6 h-6 text-white mr-2"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M5 13l4 4L19 7"
+                  ></path>
+                </svg>
+                <span className="text-2xl font-bold text-white">
+                  {messageData}
+                </span>
+              </div>
+            )}
+
             <button
-              type='button'
-              className='bg-blue-500 text-white rounded-md p-2 hover:bg-blue-800'
+              type="button"
+              className="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-800"
               onClick={() => {
                 setIsOpen(true);
                 setDataSource({});
@@ -98,7 +125,7 @@ export default function App({ dehydratedState }: AppProps) {
               <tr className="bg-gray-200">
                 <th className="px-4 py-2 text-left">Title</th>
                 <th className="px-4 py-2 text-left">Content</th>
-                <th className='px-4 py-2 text-left'>Action</th>
+                <th className="px-4 py-2 text-left">Action</th>
               </tr>
             </thead>
             <tbody>
@@ -107,13 +134,19 @@ export default function App({ dehydratedState }: AppProps) {
                   <tr key={post.id} className="border-b">
                     <td className="px-4 py-2">{post.title}</td>
                     <td className="px-4 py-2">{post.content}</td>
-                    <td className='px-4 py-2'>
-                      <button className='bg-blue-500 text-white rounded-md p-2 hover:bg-blue-800'
+                    <td className="px-4 py-2">
+                      <button
+                        className="bg-blue-500 text-white rounded-md p-2 hover:bg-blue-800"
                         onClick={() => handleEdit(post)}
-                      >Edit</button>
-                      <button className='bg-red-500 text-white rounded-md p-2 ml-2 hover:bg-red-800'
+                      >
+                        Edit
+                      </button>
+                      <button
+                        className="bg-red-500 text-white rounded-md p-2 ml-2 hover:bg-red-800"
                         onClick={() => handleDelete(post)}
-                      >Delete</button>
+                      >
+                        Delete
+                      </button>
                     </td>
                   </tr>
                 ))
@@ -130,25 +163,30 @@ export default function App({ dehydratedState }: AppProps) {
           {dataLength && data?.pagination?.totalPages > 1 && (
             <div className="flex justify-center mt-4">
               <button
-                className='bg-gray-300 text-gray-700 rounded-md p-2 hover:bg-gray-400'
+                className="bg-gray-300 text-gray-700 rounded-md p-2 hover:bg-gray-400"
                 onClick={() => handlePageChange(page - 1)}
                 disabled={page === 1}
               >
                 Previous
               </button>
               <div className="flex mx-4">
-                {pageNumbers.length > 0 && pageNumbers.map((number) => (
-                  <button
-                    key={number}
-                    className={`mx-1 p-2 rounded-md ${page === number ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-700 hover:bg-gray-400'}`}
-                    onClick={() => handlePageChange(number)}
-                  >
-                    {number}
-                  </button>
-                ))}
+                {pageNumbers.length > 0 &&
+                  pageNumbers.map((number) => (
+                    <button
+                      key={number}
+                      className={`mx-1 p-2 rounded-md ${
+                        page === number
+                          ? "bg-blue-500 text-white"
+                          : "bg-gray-300 text-gray-700 hover:bg-gray-400"
+                      }`}
+                      onClick={() => handlePageChange(number)}
+                    >
+                      {number}
+                    </button>
+                  ))}
               </div>
               <button
-                className='bg-gray-300 text-gray-700 rounded-md p-2 hover:bg-gray-400'
+                className="bg-gray-300 text-gray-700 rounded-md p-2 hover:bg-gray-400"
                 onClick={() => handlePageChange(page + 1)}
                 disabled={page === data?.pagination?.totalPages}
               >
@@ -158,12 +196,17 @@ export default function App({ dehydratedState }: AppProps) {
           )}
         </div>
       </div>
-      {isOpen && <Add isOpen={isOpen} dataSource={dataSource}
-        isMessage={isMessage}
-        messageData={messageData}
-        setIsMessage={setIsMessage}
-        setMessageData={setMessageData}
-        setIsOpen={setIsOpen} />}
+      {isOpen && (
+        <Add
+          isOpen={isOpen}
+          dataSource={dataSource}
+          isMessage={isMessage}
+          messageData={messageData}
+          setIsMessage={setIsMessage}
+          setMessageData={setMessageData}
+          setIsOpen={setIsOpen}
+        />
+      )}
     </HydrationBoundary>
   );
 }
